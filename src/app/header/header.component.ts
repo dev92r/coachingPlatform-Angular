bba1b11321903e234d9fda7fce4da6f891cff845
Devanatha R <dev92r@gmail.com>
2021-10-18 15:44:00 +0530

    Templates have been added

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { observable } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  pageTitle: string = "";
 
+  constructor(private router: Router) {
+    this.router.events
+    .subscribe(
+      (event) => {
+        if(event instanceof NavigationEnd) {
+          if(event.urlAfterRedirects == '/home') {
+            this.pageTitle = 'HOME'
+          } else if (event.urlAfterRedirects == '/about') {
+            this.pageTitle = 'ABOUT'
+          } else if (event.urlAfterRedirects == '/findyourcoach') {
+            this.pageTitle = 'FIND YOUR COACH'
+          } else if (event.urlAfterRedirects == '/contact') {
+            this.pageTitle = 'CONTACT'
+          }
+        }
+       }
+      )
+  }
+  
   ngOnInit(): void {
   }
+
 
 }
