69f2992faada9f0da8e51772e4d8ac4f2f5253e2
Devanatha R <dev92r@gmail.com>
2021-10-19 21:51:19 +0530

    configured coaches without json-server

--- src/app/services/coaches.service.ts
+++ src/app/services/coaches.service.ts
@@ -3,6 +3,9 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Coach } from '../shared/coach';
 import { baseURL } from '../shared/baseurl';
+import { COACHES } from '../shared/coaches';
+import { of } from 'rxjs';
+import { delay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +15,6 @@ export class CoachesService {
   constructor(private http: HttpClient) { }
 
   getCoaches():Observable<Coach[]>  {
-    return this.http.get<Coach[]>(baseURL + 'coaches');
+    return of(COACHES).pipe(delay(2000));
   }
 }
---
+++ src/app/shared/coaches.ts
@@ -0,0 +1,60 @@
+import { Coach } from "./coach";
+
+export const COACHES: Coach[] = [
+    {
+      "id": "0",
+      "name": "Jason maguire",
+      "intro": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "topic": "Finance",
+      "industry": "IT"
+    },
+    {
+      "id": "1",
+      "name": "Jeffery Rockensen",
+      "intro": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "topic": "Carrer Building",
+      "industry": "IT HR"
+    },
+    {
+      "id": "2",
+      "name": "Uwe Spangler",
+      "intro": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "topic": "Fitness, Motivation",
+      "industry": "Coaching"
+    },
+    {
+      "id": "3",
+      "name": "Jaden Smith",
+      "intro": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "topic": "Work-life Balance",
+      "industry": "Technologies"
+    },
+    {
+      "id": "4",
+      "name": "Will Smith",
+      "intro": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "topic": "Motivation",
+      "industry": "Coaching"
+    },
+    {
+      "id": "5",
+      "name": "Devanatha",
+      "intro": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "topic": "Travelling, Stress busters",
+      "industry": "Tourism"
+    },
+    {
+      "id": "6",
+      "name": "Samurai Jack",
+      "intro": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "topic": "Patience",
+      "industry": "IT"
+    },
+    {
+      "id": "7",
+      "name": "Kapil Dev",
+      "intro": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "topic": "Life coaching, Personal Coaching",
+      "industry": "Coaching"
+    }
+  ]
